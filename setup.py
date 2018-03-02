651c6c33649c9ff33f4a2d1c5521707eaec531da
Renato Orgito <orgito@gmail.com>
2018-03-02 14:43:42 -0300

    release 0.0.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='uclcoin',
-    version='0.0.5',
+    version='0.0.6',
     description='UCLCoin: A naive blockchain/cryptocurrency implementation',
     long_description=open('README.rst', 'r').read(),
     author='Renato Orgito',
